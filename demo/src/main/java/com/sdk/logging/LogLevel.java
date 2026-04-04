811026c6c4e90c36a0df60d0a3264e3f9e0f7010
solutiontech214 <onkargutti94@gmail.com>
2026-04-05 02:25:00 +0530

    bugs removed

--- demo/src/main/java/com/sdk/logging/LogLevel.java
+++
@@ -1,9 +0,0 @@
-package com.sdk.logging;
-
-public enum LogLevel {
-    INFO,
-    ERROR,
-    WARN,
-    DEBUG,
-    FATAL
-}
--- demo/src/main/java/com/sdk/logging/SdkConfig.java
+++
@@ -1,9 +0,0 @@
-package com.sdk.logging;
-
-public class SdkConfig {
-
-    public String projectId;
-    public String moduleId;
-    public String submoduleId;
-    public String environment;
-}
